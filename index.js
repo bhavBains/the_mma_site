aae9f74b00e779c63fbd13516f4fd7b44bced748
bhavBains <deepbains26@gmail.com>
2020-04-16 15:32:22 -0700

    more clean work on master branch

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 // Initiate app
 const app = express();
+
 // Booting up server on port 5000
 app.listen(5000, () => console.log("App is running at port 5000"));
