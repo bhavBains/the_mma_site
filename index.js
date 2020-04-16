42e877f266014b8fc825481e1e6c01638e3ffa61
bhavBains <deepbains26@gmail.com>
2020-04-16 15:01:11 -0700

    clean up master branch of test code

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const { buildSchema } = require("graphql");
-
+// Initiate app
 const app = express();
-
-const schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-const root = {
-  hello: () => {
-    return "Hello World";
-  },
-};
-
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-  })
-);
-
+// Booting up server on port 5000
 app.listen(5000, () => console.log("App is running at port 5000"));
