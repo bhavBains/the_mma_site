178d9a64afad7e3c935bbd1800e4fefd484815e3
bhavBains <deepbains26@gmail.com>
2020-04-18 16:53:20 -0700

    fixed errors in index.js

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
 const app = require("./server/server");
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 const PORT = process.env.PORT || 5000;
 // Booting up server
 app.listen(PORT, () => console.log(`App is running at port ${PORT}`));
--- server/server.js
+++ server/server.js
@@ -4,8 +4,6 @@ const schema = require("./schema/schema");
 // Initiate app
 const app = express();
 
-module.exports = app;
-
 app.use(
   "/graphql",
   expressGraphQL({
@@ -13,3 +11,14 @@ app.use(
     graphiql: true,
   })
 );
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+module.exports = app;
