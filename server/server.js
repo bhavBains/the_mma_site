500d861c9de327ba8e152f75ab3eae062662458f
bhavBains <deepbains26@gmail.com>
2020-04-23 12:04:11 -0700

    server.js and package.json changes

--- server/server.js
+++ server/server.js
@@ -23,7 +23,11 @@ app.use(
 
 module.exports = app;
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000/",
+    "https://themmasite.herokuapp.com/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
