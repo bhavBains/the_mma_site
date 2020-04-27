6598e3e35ace7c35c27808d91e2eaaaae66dc663
bhavBains <deepbains26@gmail.com>
2020-04-27 14:27:39 -0700

    server.js code add

--- server/server.js
+++ server/server.js
@@ -12,14 +12,14 @@ app.use(
   })
 );
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 module.exports = app;
 app.use(function (req, res, next) {
