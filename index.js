7efa19412c1d62427604d4650247847c56bd46b8
bhavBains <deepbains26@gmail.com>
2020-05-18 11:20:55 -0700

    local server port changed to 8000

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require("./server/server");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 // Booting up server
 app.listen(PORT, () => console.log(`App is running at port ${PORT}`));
